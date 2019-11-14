525648e9b921b9086825ea33c8088a4ead87eb78
Gabriel França <dev.gabrielfranca@gmail.com>
2019-11-13 23:57:38 -0300

    Finishing repositories list

---
+++ src/pages/Main/index.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton, List } from './styles';
+
+export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const repositoriesStorage = localStorage.getItem('repositories');
+
+    if (repositoriesStorage) {
+      setRepositories(JSON.parse(repositoriesStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('repositories', JSON.stringify(repositories));
+  }, [repositories]);
+
+  const handleInputChange = e => {
+    setNewRepo(e.target.value);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    setRepositories([...repositories, data]);
+    setNewRepo('');
+    setLoading(false);
+  };
+
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
+
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+
+      <List>
+        {repositories.map(repositorie => (
+          <li key={repositorie.name}>
+            <span>{repositorie.name}</span>
+            <Link to={`/repository/${encodeURIComponent(repositorie.name)}`}>
+              Detalhes
+            </Link>
+          </li>
+        ))}
+      </List>
+    </Container>
+  );
+}
